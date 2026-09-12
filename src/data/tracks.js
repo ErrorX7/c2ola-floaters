081da8ae47d05603525ab3d219c37f5b8edbec96
ErrorX7 <154410688+ErrorX7@users.noreply.github.com>
2026-09-12 13:12:15 +0800

    Refresh grass reveal script

--- src/data/tracks.js
+++ src/data/tracks.js
@@ -1,6 +1,6 @@
 import "../../wilds-reel-song.js?v=20260912c";
 import "../../goodnews-wave.js?v=20260912b";
-import "../../grass-puzzle-fix.js?v=20260912b";
+import "../../grass-puzzle-fix.js?v=20260912c";
 import "../../author-letter.js?v=20260912a";
 import "../../brand-credit.js?v=20260912b";
 
